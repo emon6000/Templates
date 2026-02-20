328cba67bab2119a427850dab5a8f1050905e5b2
emon6000 <abdullah6377@gmail.com>
2026-02-20 16:42:37 +0600

    updated

--- loginform/frontend/src/Components/Pages/Home.jsx
+++ loginform/frontend/src/Components/Pages/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://login-register-logout.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password })
@@ -44,7 +44,7 @@ const Home = () => {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/api/auth/register", {
+      const response = await fetch("https://login-register-logout.onrender.com/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password })
